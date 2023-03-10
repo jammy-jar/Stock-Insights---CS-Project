856f60d3d89428c11142b7c60d792931aea5039a
vaindev <vaindev.jar@gmail.com>
2023-03-10 15:20:27 +0000

    changes

--- public/javascripts/load-data.js
+++ public/javascripts/load-data.js
@@ -1,62 +1,114 @@
-let pageNum = 0;
 const tableBody = document.querySelector("#stocksTable")
 
-// Declare a function that iterates through the data and graphs the data for each stock.
-const createPlot = (canvas, symbol, data) => {
-    // Create a canvas context.
-    const ctx = canvas.getContext('2d');
-
-    // Set the line width for the plot.
-    ctx.lineWidth = 3;
-
-    // Check if the first data value is less than the final data value.
-    // If its smaller the line is green to indicate there has been an increase in value over the past year,
-    // And if its greater (or equal to) then its red to indicate that there has been a loss or no increase.
-    if (data[0].close < data[data.length - 1].close) {
-        ctx.strokeStyle = 'green';
-    } else {
-        ctx.strokeStyle = 'red';
-    }
-
-    // Set the scale for the plot.
-    const xScale = canvas.width / data.length;
-    const yScale = canvas.height / Math.max(...data.map(({close}) => close));
-
-    // Begin a new path.
-    ctx.beginPath();
-
-    // Iterate through the data and draw the plot.
-    for (let i = 0; i < data.length; i += 2) {
-        const x = i * xScale;
-        const y = canvas.height - data[i].close * yScale;
-        ctx.lineTo(x, y);
-    }
-
-    // Stroke the path to draw the plot on the canvas.
-    ctx.stroke();
+const runClickable = () => {
+    // jQuery executed when a element with class 'clickable-row' is clicked,
+    // and sets the users window location './{Stock Symbol}'.
+    jQuery(document).ready(function($) {
+        $(".clickable-row").click(function() {
+            window.location = $(this).data('href');
+        });
+    });
 }
 
-const listStocks = async pageNum => {
-    const selectStocks = stocks.slice(pageNum*20, pageNum*20 + 20)
-    for (const stock of selectStocks) {
+// Function displays stocks in the table on the index page.
+const listStocks = stocks => {    
+    // Iterate through the selected stocks.
+    for (const stock of stocks) {
+        // Create a new row, with the 'clickable-row' class, 
+        // and a data-href of the stocks symbol.
         const row = tableBody.insertRow(-1)
+        row.classList.add('clickable-row', 'align-middle')
+        $(row).attr('data-href', stock.symbol);
+
+        // Create a new canvas element, and plot the data with the 'createIndexPlot' function.
         const plotCanvas = document.createElement("CANVAS");
-        row.insertCell(0).append(stock.symbol)
+        plotCanvas.classList.add('w-75')
+        createIndexPlot(plotCanvas, stock.symbol, stock.data)
+
+        // Insert cells with the stock's data.
+        const rowHeader = document.createElement('th')
+        rowHeader.setAttribute('scope', 'row')
+        rowHeader.append(stock.symbol)
+
+        const price = stock.price > 1000 ? Math.round(stock.price) : stock.price.toPrecision(4);
+
+        row.append(rowHeader)
         row.insertCell(1).append(stock.name)
-        row.insertCell(2).append(stock.price)
+        row.insertCell(2).append(price)
         row.insertCell(3)
         row.insertCell(4)
         row.insertCell(5).append(plotCanvas)
-
-        createPlot(plotCanvas, stock.symbol, stock.data)
     }
+
+    runClickable();
 }
-listStocks(pageNum);
 
-addEventListener('scroll', evt => {
-    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
-        pageNum++;
-        listStocks(pageNum);
+// Function displays stocks in the table on the watchlist page.
+const listWatchedStocks = () => {
+
+    // Iterate through the selected stocks.
+    for (const stock of stocks) {
+        // Create a new row, with the 'clickable-row' class, 
+        // and a data-href of the stocks symbol.
+        const row = tableBody.insertRow(-1)
+        row.classList.add('clickable-row', 'align-middle')
+        $(row).attr('data-href', stock.symbol);
+
+        // Calculate the percentage change for the stock, by comparing the value when it was
+        // first watched, to the value now.
+        let percentageChange;
+        user.watchlist.forEach(item => {
+            if (item[0] == stock.symbol) {
+                percentageChange = (100 * ((stock.price / item[2]) - 1)).toPrecision(2) + '%';
+            }
+        }) 
+
+        // Create a form, with a button.
+        // Add a name attribute to the button, along with a value attribute, this is to fetch
+        // information from the button via the server-side watchlist route when posted.
+        const button = document.createElement('button')
+        const form = document.createElement('form')
+        button.classList.add('btn', 'btn-primary')
+        button.setAttribute('name', 'stockSymbol')
+        button.setAttribute('value', stock.symbol)
+        button.append('Remove')
+
+        // Add an action attribute, and a method attribute, to tell the form to POST to
+        // 'watchlist/remove' when submitted.
+        form.setAttribute('action', '/watchlist/remove')
+        form.setAttribute('method', 'POST')
+
+        // Add the button to the form.
+        form.append(button)
+
+        // Insert cells with the stock's data, and the 'remove' button/ form.
+        // Insert cells with the stock's data.
+        const rowHeader = document.createElement('th')
+        rowHeader.setAttribute('scope', 'row')
+        rowHeader.append(stock.symbol)
+
+        const price = stock.price > 1 ? Math.round(stock.price) : stock.price.toPrecision(2);
+
+        row.append(rowHeader)
+        row.insertCell(1).append(stock.name)
+        row.insertCell(2).append(price)
+        row.insertCell(3)
+        row.insertCell(4)
+        row.insertCell(5).append(percentageChange)
+        row.insertCell(6).append(form)
     }
-})
 
+    runClickable();
+}
+
+const getStocks = async () => {
+    let res;
+    if (window.location.search) {
+        res = await fetch(
+            `http://localhost:8080/list${window.location.search}&pageNum=${pageNum}`
+            );
+    } else {
+        res = await fetch(`http://localhost:8080/list?pageNum=${pageNum}`);
+    }
+    return await res.json();
+}
